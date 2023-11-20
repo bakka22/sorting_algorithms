b71b337b387cb21c67a9d4c88027730a54e30b31
bakka22 <abobakereltom@gmail.com>
2023-11-20 07:03:08 +0000

    s"

---
+++ 100-shell_sort.c
@@ -0,0 +1,57 @@
+#include "sort.h"
+#include <stdio.h>
+/**
+*swap2 - swap two elements in an array
+*@array: array used
+*@idx1: index of the first element
+*@idx2: index of the second element
+*Return: nothing
+*/
+void swap2(int *array, int idx1, int idx2)
+{
+	int tmp;
+
+	tmp = array[idx1];
+	array[idx1] = array[idx2];
+	array[idx2] = tmp;
+}
+/**
+*shell_sort - sort an array using shell sort
+*@array: array to be sorted
+*@size: size of the array
+*Return: nothing
+*/
+void shell_sort(int *array, size_t size)
+{
+	int gap = 1, i, j;
+
+	if (!array || size < 2)
+		return;
+
+	while (gap < (int)size / 3)
+	{
+		gap = gap * 3 + 1;
+	}
+	while (gap)
+	{
+		for (i = 0; i < (int)size; i++)
+		{
+			if (i + gap < (int)size)
+			{
+				if (array[i + gap] < array[i])
+				{
+					swap2(array, i, i + gap);
+				}
+				for (j = i; j - gap > 0; j = j - gap)
+				{
+					if (array[j] < array[j - gap])
+						swap2(array, j, j - gap);
+				}
+			}
+			else
+				break;
+		}
+		gap /= 3;
+		print_array(array, size);
+	}
+}
